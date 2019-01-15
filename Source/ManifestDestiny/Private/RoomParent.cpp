5f95130184d9c62c851480ab296640ae516ab4d9
jdmajernik <jdmajernik@gmail.com>
2019-01-15 06:00:04 -0500

    Pre-build bug fixes

---
+++ Source/ManifestDestiny/Private/RoomParent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoomParent.h"
+
+
+// Sets default values
+ARoomParent::ARoomParent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARoomParent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogActor, Warning, TEXT("Hello World!"));
+	
+}
+
+// Called every frame
+void ARoomParent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
